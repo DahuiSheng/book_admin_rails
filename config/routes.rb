f7008712d0502114e092d6aff56fa827b7c82b53
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-07-06 02:46:36 +0900

    routesにonlyで制限を適用する

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/books/:id" => "books#show"
   delete "books/:id" => "books#destroy"
   resources :publishers
+  resources :profile, only: %i{show edit update}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
