f809f20536f7e3def1deb15c0df0c3edd312def2
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-07-06 02:39:17 +0900

    ルーティングにPublisherのリソースを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/books/:id" => "books#show"
   delete "books/:id" => "books#destroy"
+  resources :publishers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
